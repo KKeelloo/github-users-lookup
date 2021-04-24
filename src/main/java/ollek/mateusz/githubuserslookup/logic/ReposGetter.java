53869c7659c1074c03c2084aabd660baf5accb27
Mateusz Ollek <mateuszollek@gmail.com>
2021-04-24 14:13:45 +0200

    Fixed counting

--- src/main/java/ollek/mateusz/githubuserslookup/logic/ReposGetter.java
+++ src/main/java/ollek/mateusz/githubuserslookup/logic/ReposGetter.java
@@ -11,35 +11,91 @@ import java.util.List;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReposGetter {
     private String user_name;
     private List<RepoHolder> repos;
-
+    private static final Logger logger = LoggerFactory.getLogger(ReposGetter.class);
     public ReposGetter(String user_name){
         this.user_name = user_name;
         repos = null;
     }
 
-    public void searchForRepos(){
+    private HttpURLConnection createConnection(URL url) throws IOException{
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setConnectTimeout(5000);
+        con.setReadTimeout(5000);
+        return con;
+    }
+
+    private void errorThrower(HttpURLConnection con, int status) throws Exception{
+        if(status==404){
+            con.disconnect();
+            throw new Exception("Make sure username is correct");
+        }else{
+            logger.info("message:" + con.getResponseMessage());
+            if(con.getResponseMessage().compareTo("rate limit exceeded")==0){
+                con.disconnect();
+                throw new Exception("Looks like rate limit is exceeded, try again later");
+            }
+            con.disconnect();
+            throw new Exception("Something unexpected happened");
+        }
+    }
+
+    public void searchForRepos() throws Exception{
         try {
-            URL url = new URL("https://api.github.com/users/" + user_name + "/repos");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setConnectTimeout(5000);
-            con.setReadTimeout(5000);
+            Integer number_of_repos = this.numberOfRepos();
+            if(number_of_repos==null){
+                return;
+            }
+            logger.info(user_name+" should have " + number_of_repos + " repos");
+            int rcv_counter = 1;
+            while ((rcv_counter-1)*100 < number_of_repos) {
+                URL url = new URL("https://api.github.com/users/" + user_name + "/repos?per_page=100&page=" + rcv_counter);
+                HttpURLConnection con = createConnection(url);
+                int status = con.getResponseCode();
+                if (status == 200) {
+                    String responseString = convertStreamToString(con.getInputStream());
+                    ObjectMapper mapper = new ObjectMapper();
+                    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                    if (repos == null)
+                        repos = mapper.readValue(responseString, new TypeReference<List<RepoHolder>>() {
+                        });
+                    else
+                        repos.addAll(mapper.readValue(responseString, new TypeReference<List<RepoHolder>>() {
+                        }));
+                    con.disconnect();
+                }else
+                    errorThrower(con, status);
+                rcv_counter++;
+            }
+            logger.info(user_name+ " has " + repos.size() + " repos");
+        }catch (IOException e){
+            throw new Exception("Something went wrong while getting results this is bad");
+        }
+    }
+
+    private Integer numberOfRepos() throws Exception{
+        try {
+            URL url = new URL("https://api.github.com/users/" + user_name);
+            HttpURLConnection con = createConnection(url);
             int status = con.getResponseCode();
             if(status == 200){
                 String responseString = convertStreamToString(con.getInputStream());
-                ObjectMapper mapper = new ObjectMapper();
-                mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-                repos = mapper.readValue(responseString, new TypeReference<List<RepoHolder>>() { });
-            }
-            con.disconnect();
-        }catch (Exception e){}
-
+                con.disconnect();
+                JSONObject jsonObject = (JSONObject) JSONValue.parse(responseString);
+                return Integer.parseInt(jsonObject.get("public_repos").toString());
+            }else
+                errorThrower(con, status);
+        }catch (IOException e){}
+        return null;
     }
 
     private static String convertStreamToString(InputStream is) {
@@ -63,8 +119,10 @@ public class ReposGetter {
         return sb.toString();
     }
 
-    public StarsCountResponseHolder calcStarsCount(){
+    public StarsCountResponseHolder calcStarsCount() throws Exception{
         this.searchForRepos();
+        if(repos == null)
+            return null;
         StarsCountResponseHolder starsCountResponseHolder = new StarsCountResponseHolder(user_name);
         for(RepoHolder repo: repos){
             starsCountResponseHolder.updateStargazers_count(repo.getStargazers_count());
--- src/main/java/ollek/mateusz/githubuserslookup/rest/AppController.java
+++ src/main/java/ollek/mateusz/githubuserslookup/rest/AppController.java
@@ -3,6 +3,8 @@ package ollek.mateusz.githubuserslookup.rest;
 import ollek.mateusz.githubuserslookup.logic.ReposGetter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,25 +13,32 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import ollek.mateusz.githubuserslookup.logic.StarsCountResponseHolder;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @RestController
 public class AppController {
     private static final Logger logger = LoggerFactory.getLogger(AppController.class);
 
     @GetMapping("/starscount/{user}")
-    public EntityModel<StarsCountResponseHolder> starsCount(@PathVariable String user){
-        logger.info(user);
+    public EntityModel<StarsCountResponseHolder> starsCount(@PathVariable String user) throws Exception{
+        logger.info("Fetch results for "+user);
         StarsCountResponseHolder response = new ReposGetter(user).calcStarsCount();
         return EntityModel.of(response,
                 linkTo(methodOn(AppController.class).starsCount(user)).withSelfRel());
     }
 
     @GetMapping("/repos/{user}")
-    public EntityModel<ReposGetter> repos(@PathVariable String user){
-        logger.info(user);
+    public EntityModel<ReposGetter> repos(@PathVariable String user) throws Exception {
+        logger.info("Fetch results for "+user);
         ReposGetter response = new ReposGetter(user);
         response.searchForRepos();
         return EntityModel.of(response,
                 linkTo(methodOn(AppController.class).repos(user)).withSelfRel());
+    }
+
+    @ExceptionHandler({Exception.class})
+    public String errorHandler(HttpServletRequest req, Exception ex){
+        return ex.getMessage();
     }
 }
